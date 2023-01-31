d3eaca1670f9fd741fae585a3d906633216ccb62
nassim <linnnux@hotmail.fr>
2023-01-31 15:57:43 +0100

    :v 0.0.3 : update displayed message

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Hello les P3A2 Dimitri!');
 }).listen(8080);
