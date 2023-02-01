d1b5a2a17236bcfbfab2dab407aeaec4d311eeb6
nassim <linnnux@hotmail.fr>
2023-02-01 12:54:43 +0100

    :v 0.0.9 : update wellecome message

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var http = require('http');
 http.createServer(function (req, res)
 {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello les P3A2 !  Dimitri ! we are the 1st february');
+  res.end('Hello les P3A2 !  Dimitri ! we are the 1st february, are you ok Dimitri?');
 }).listen(8080);
